ab0d849f6ea6dff18449c5b9fad3675a3d226376
Diana Percatkina <74015697+cov393@users.noreply.github.com>
2024-10-15 15:36:58 +0100

    Update psh.ps1

--- psh.ps1
+++ psh.ps1
@@ -2,20 +2,31 @@ while ($true) {
     if (Test-Connection google.com -Quiet) {
         Write-Host "Conected" -ForegroundColor Green
 
-        $length = Get-Random -Minimum 5 -Maximum 20
-        $chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
-        $randomString = -join ((1..$length) | ForEach-Object { $chars[Get-Random -Minimum 0 -Maximum $chars.Length] })
+        # Генерация случайной строки из цифр и букв
+        $length = Get-Random -Minimum 50 -Maximum 200
+        $chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789qazxswedcvfrtgbnhyujmkiolp.@~'.ToCharArray()
         
-
+        # Создаем случайную строку
+        $randomString = ""
+        for ($i = 0; $i -lt $length; $i++) {
+            # Генерация случайного индекса для выбора символа
+            $randomIndex = Get-Random -Minimum 0 -Maximum $chars.Count
+            $randomChar = $chars[$randomIndex]
+            $randomString += $randomChar
+        }
+        
+        # Генерация случайных цветов для каждого символа
         $colors = @("Red", "Green", "Yellow", "Blue", "Magenta", "Cyan", "White")
         foreach ($char in $randomString.ToCharArray()) {
-            $color = $colors[Get-Random -Minimum 0 -Maximum $colors.Length]
+            # Генерация случайного индекса для выбора цвета
+            $colorIndex = Get-Random -Minimum 0 -Maximum $colors.Count
+            $color = $colors[$colorIndex]
             Write-Host $char -NoNewline -ForegroundColor $color
         }
-        Write-Host ""  
+        Write-Host ""  # Перевод на новую строку
     } else {
         Write-Host "Disconected" -ForegroundColor Red
     }
-    
+
     Start-Sleep -Seconds 5
 }
